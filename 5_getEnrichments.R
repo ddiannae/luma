1a95ec75b1a5063a3741194aafbaa0fc7ddea0b3
Diana Elisa García Cortés dgarcia@inmegen.edu.mx <dgarcia@inmegen.edu.mx>
2020-05-07 15:35:46 +0000

    Adding enrichment script and results

--- 5_getEnrichments.R
+++ 5_getEnrichments.R
@@ -1,45 +1,71 @@
 library(readr)
 library(dplyr)
-#library(parallel)
-# No paralllel
-## Aqui porque: https://support.bioconductor.org/p/38541/
-library(clusterProfiler)
+library(parallel)
+library(DBI)
 library(org.Hs.eg.db)
+library(clusterProfiler)
 
 conds <- c("healthy", "luma")
 
-gene_list <- lapply(conds, function(cond) {
-  genes <- read_tsv(file = paste0("data/network-tables/", cond,  "-vertices.tsv"))
-  return(genes$ensemblID)
-})
-gene_list <- unique(unlist(gene_list))
+## https://github.com/YuLab-SMU/clusterProfiler/issues/207
+## Creamos el objeto GO_BP porque: https://support.bioconductor.org/p/38541/
+
+## esta funcion la adaptamos de la funcion annFUN.org del paquete topGO
+## no podemos usar topGO porque no puede paralelizarse
+getGO_DB <- function() {
+  .sql <- paste("SELECT DISTINCT ensembl_id, go_id FROM ensembl",
+                " INNER JOIN go_bp", 
+                " USING(_id)", sep = "")
+  retVal <- dbGetQuery(get("org.Hs.eg_dbconn")(), .sql)
+  
+  ## split the table into a named list of GOs
+  return(split(retVal[["ensembl_id"]], retVal[["go_id"]]))
+}
+
+GO_BP <- getGO_DB()
+GO_BP <- tibble::enframe(GO_BP)
 
 m <- lapply(conds, function(cond) {
   gene_sets <- read_tsv(file = paste0("data/", cond,  "-communities.tsv"))
+  gene_universe <- read_tsv(file = paste0("data/network-tables/", cond,  "-vertices.tsv"))
+  gene_universe <- unlist(gene_universe$ensemblID)
 
-  all_enrichments <-lapply(unique(gene_sets$community), 
+  all_enrichments <- mclapply(X = unique(gene_sets$community), 
+                              mc.cores = 75,
                               FUN = function(com){
    
       geneList <- unlist(gene_sets %>% filter( community == com) %>%
                            dplyr::select(ensemblID))
-      geneList <- setNames(factor(as.integer(gene_list %in% geneList)), gene_list)
-      GOdata <- new("topGOdata",
-                    description = "GO analysis of genes in community",
-                    ontology = "BP",
-                    allGenes = geneList,
-                    nodeSize = 10,
-                    annot = annFUN.org,
-                    ID = "Ensembl", 
-                    mapping = "org.Hs.eg")
       
-      resultFisher <- runTest(GOdata, "elim","fisher")
-      ss <- score(resultFisher)
-    
-      cat("Community ", com, " has ", sum(ss < 0.001), " significant processes")
-      resultsTable <- GenTable(GOdata, classicFisher = resultFisher, numChar = 200,
-                                 topNodes = sum(ss < 0.001))
-      resultsTable$commun <- com
-      return(resultsTable)
+      ego <- enricher(gene          = geneList,
+                      universe      = gene_universe,
+                      pAdjustMethod = "BH",
+                      pvalueCutoff  = 0.01,
+                      qvalueCutoff  = 0.05,
+                      minGSSize     = 10,
+                      TERM2GENE     = GO_BP)
+      
+      if(nrow(as.data.frame(ego)) > 0) {
+        ego@ontology = "BP"
+        ego@organism = "Homo sapiens"
+        
+        ego <- as.data.frame(simplify(
+          ego,
+          cutoff = 0.7,
+          by = "p.adjust",
+          select_fun = min,
+          measure = "Wang",
+          semData = NULL
+        ))
+        
+        ego <- ego %>% inner_join(go2term(ego$ID), by = c("ID" = "go_id")) %>% 
+                dplyr::select(-Description) %>% dplyr::select(ID, Term, everything())
+        
+        
+        ego$commun <- com
+        return(ego)  
+      }
+      return(NULL)
   })
   
   all_enrichments <- plyr::compact(all_enrichments) 
@@ -48,5 +74,4 @@ m <- lapply(conds, function(cond) {
   write.table(all_enrichments, file =  paste0("data/", cond,  "-enrichments.tsv"),
               quote = F, row.names = F, sep = "\t")
 })
-
 
