aaa70756491a4d23140a1abd72c0d726eccb4aa8
Diana Elisa García Cortés dgarcia@inmegen.edu.mx <dgarcia@inmegen.edu.mx>
2020-06-30 12:11:40 -0500

    Adding scripts and data for ctcf binding sites exploration

--- ctcfs_exploration.R
+++ ctcfs_exploration.R
@@ -6,38 +6,26 @@ library(ggthemes)
 library(RColorBrewer)
 library(scales)
 
-annot <- read_tsv("../data/Biomart_EnsemblG94_GRCh38_p12_karyotype.txt",
-                    col_names = c("ensemblID", "chr", "start", "end", "karyoband"),
+annot <- read_tsv("data/Biomart_EnsemblG94_GRCh38_p12_karyotype.txt",
+                    col_names = c("ensembl_id", "chr", "start", "end", "karyoband"),
                     col_types = cols(col_character(), col_character(), col_integer(), 
                                      col_integer(), col_character()),
                     skip = 1)
-genes <- read_tsv("../data/genes_in_exp_matrix.txt", col_names = "ensemblID")
+genes <- read_tsv("data/genes_in_exp_matrix.txt", col_names = "ensembl_id")
 
 annot <- annot %>% semi_join(genes)
 
-ctcfs <- read_tsv("../data/non_gene_overlaping_ctcfbs.tsv", 
-                  col_types = cols(col_character(), col_integer(), col_integer()))
+ctcfs <- read_tsv("data/non_gene_overlaping_ctcfbs.tsv", 
+                  col_types = cols(col_character(), col_integer(), col_integer(),
+                                   col_character(), col_double()))
 ctcfs <- ctcfs[!duplicated(ctcfs), ]
 
 getPalette = colorRampPalette(brewer.pal(9, "Set1"))
 
-gene_counts <- annot %>% group_by(chr) %>% tally()
-gene_counts$chr <- factor(gene_counts$chr, levels = as.character(c(seq(1:22), "X")))
-
-png(filename = "../figures/gene_count.png", width = 1200, height = 600)
-ggplot(gene_counts, aes(x = chr, y = n, fill = chr)) +
-  geom_bar(stat = "identity") +
-  theme_few(base_size = 20) +
-  theme(legend.position = "none") +
-  ylab("Genes") +
-  xlab("Chromosome") +
-  scale_fill_manual(values = getPalette(23))
-dev.off()
-
 ctcfs_counts <- ctcfs %>% group_by(chr) %>% tally()
 ctcfs_counts$chr <- factor(ctcfs_counts$chr, levels = as.character(c(seq(1:22), "X")))
 
-png(filename = "../figures/ctcf_count.png", width = 1200, height = 600)
+png(filename = "figures/ctcf_count.png", width = 1200, height = 600)
 ggplot(ctcfs_counts, aes(x = chr, y = n, fill = chr)) +
   geom_bar(stat = "identity") +
   theme_few(base_size = 20) +
@@ -49,44 +37,44 @@ dev.off()
 
 chrs <- as.character(c(seq(1:22), "X"))
 
-gb_ctfs <- parallel::mclapply(X = chrs, mc.cores = 23, FUN = function(c){
-  gs_in_c <- annot %>% filter(chr == c)
-  cs_in_c <- ctcfs %>% filter(chr == c) 
-  cs_in_c <- add_row(cs_in_c, chr = c, start = 0, end = 0) %>% arrange(start)
+gb_ctfs <- parallel::mclapply(X = chrs, mc.cores = 23, FUN = function(ch){
+  gs_in_c <- annot %>% filter(chr == ch) %>% 
+    select(-karyoband) %>%
+    dplyr::rename(id = ensembl_id) %>%
+    mutate(type = "Gene")
+  cs_in_c <- ctcfs %>% filter(chr == ch) %>%
+    select(-score)%>%
+    mutate(type = "CTCF")
   
-  genes <- lapply(seq(1:(nrow(cs_in_c)-1)), function(i){
-    s <- unlist(cs_in_c[i, "end"])
-    e <- unlist(cs_in_c[i+1, "start"])
+  all <- bind_rows(gs_in_c, cs_in_c) %>% arrange(start) %>%
+    mutate(gene_sum = cumsum(type == "Gene"))
+  cs_in_c <- all %>% filter(type == "CTCF")
+  
+  ## Check how many genes are between two ctcf binding sites
+  ngenes <- lapply(seq(1:nrow(cs_in_c)), function(i){
+    cs <- unname(unlist(cs_in_c[i, "gene_sum"]))
+    start <- unname(unlist(cs_in_c[i, "start"]))
+    pre_cs <- unname(unlist(cs_in_c[i-1, "gene_sum"]))
+    pre_end <- unname(unlist(cs_in_c[i-1, "end"]))
     
-    ngenes <- gs_in_c %>% filter(start >= s & end <= e) %>% nrow()
-    return(list(i = i, ngenes = ngenes, dist = e - s))
+    return(list(genes = cs - pre_cs, distance = start - pre_end))
   })
-  genes <- bind_rows(genes)
-  s <- max(cs_in_c$end)
-  e <- max(gs_in_c$end)
-  ngenes <- gs_in_c %>% filter(start >= s & end <= e) %>% nrow()
-  genes <- genes %>% add_row(i = nrow(cs_in_c), ngenes = ngenes, dist = e - s)
-  genes$cum_sum <- cumsum(genes$ngenes)
-  cs_in_c <- bind_cols(cs_in_c, genes)
+  cs_in_c$genes <- c(cs_in_c$gene_sum[1], unlist(lapply(ngenes, "[[", "genes")))
+  cs_in_c$distance <- c(cs_in_c$start[1], unlist(lapply(ngenes, "[[", "distance")))
   
   return(cs_in_c)
 })
 
-gb_ctfs <- bind_rows(gb_ctfs)
+gb_ctfs <- bind_rows(gb_ctfs) %>% select(-type)
 
-e_starts <- gb_ctfs %>% group_by(chr) %>% filter( i == 1)
-e_ends <- gb_ctfs %>% group_by(chr) %>% filter( i == max(i))
-g_betw <- gb_ctfs %>% group_by(chr) %>% filter( i > 1 & i < max(i))
-g_betw$chr <- factor(g_betw$chr, levels = as.character(c(seq(1:22), "X")))
-
-ceros <- g_betw %>% filter(ngenes == 0) %>% group_by(chr) %>% tally()
+ceros <- gb_ctfs %>% filter(genes == 0) %>% group_by(chr) %>% tally()
 ceros$empty = "Empty"
-noceros <- g_betw %>% filter(ngenes != 0) %>% group_by(chr) %>% tally()
+noceros <- gb_ctfs %>% filter(genes != 0) %>% group_by(chr) %>% tally()
 noceros$empty = "Non Empty"
 
 ctcfs_counts <- bind_rows(ceros, noceros)
 
-png(filename = "../figures/ctcf_count_empty.png", width = 1200, height = 600)
+png(filename = "figures/ctcf_count_empty.png", width = 1200, height = 600)
 ggplot(ctcfs_counts, aes(x = chr, y = n)) +
   geom_bar(
     aes(color = empty, fill = empty),
@@ -100,9 +88,9 @@ ggplot(ctcfs_counts, aes(x = chr, y = n)) +
   scale_color_tableau() 
 dev.off()
 
-noceros <- g_betw %>% filter(ngenes != 0) 
-png(filename = "../figures/ctcf_genes_between_nonempty.png", width = 1200, height = 600)
-ggplot(noceros, aes(x = chr, y = ngenes, fill = chr)) +
+noceros <- gb_ctfs %>% filter(genes != 0) 
+png(filename = "figures/ctcf_genes_between_nonempty.png", width = 1200, height = 600)
+ggplot(noceros, aes(x = chr, y = genes, fill = chr)) +
   geom_boxplot() +
   theme_few(base_size = 20) +
   theme(legend.position = "none") +
@@ -111,8 +99,8 @@ ggplot(noceros, aes(x = chr, y = ngenes, fill = chr)) +
   scale_fill_manual(values = getPalette(23))
 dev.off()
 
-png(filename = "../figures/ctcf_distance_all.png", width = 1200, height = 600)
-ggplot(g_betw, aes(x = chr, y = dist, fill = chr)) +
+png(filename = "figures/ctcf_distance_all.png", width = 1200, height = 600)
+ggplot(gb_ctfs, aes(x = chr, y = distance, fill = chr)) +
   geom_boxplot() +
   theme_few(base_size = 20) +
   theme(legend.position = "none") +
@@ -122,10 +110,4 @@ ggplot(g_betw, aes(x = chr, y = dist, fill = chr)) +
   scale_fill_manual(values = getPalette(23))
 dev.off()
 
-write_tsv(gb_ctfs, path = "../data/all_ctcfs.tsv")
-
-##### Cumulative sum to remove ctcfs with no genes
-unique_ctcfs <- gb_ctfs %>% distinct(chr, cum_sum, .keep_all = TRUE)
-unique_ctcfs$id <- paste0("CTCF", stringi::stri_pad_left(rownames(unique_ctcfs), 5, 0))
-unique_ctcfs <- unique_ctcfs %>% select(id, chr:cum_sum)
-write_tsv(unique_ctcfs, path = "../data/significant_ctcfs.tsv")
+write_tsv(gb_ctfs, path = "data/all_ctcfs.tsv")
--- get_non_overlapping_ctcfs.R
+++ get_non_overlapping_ctcfs.R
@@ -1,31 +1,28 @@
 library(readr)
-library(plyr)
 library(dplyr)
-library(tidyr)
+library(plyr)
 library(stringr)
 
-annot <- read_tsv("../data/Biomart_EnsemblG94_GRCh38_p12_karyotype.txt",
-                  col_names = c("ensemblID", "chr", "start", "end", "karyoband"),
+annot <- read_tsv("data/Biomart_EnsemblG94_GRCh38_p12_karyotype.txt",
+                  col_names = c("ensembl_id", "chr", "start", "end", "karyoband"),
                   col_types = cols(col_character(), col_character(), col_integer(), 
                                    col_integer(), col_character()),
                   skip = 1)
-genes <- read_tsv("../data/genes_in_exp_matrix.txt", col_names = "ensemblID")
-
-annot <- annot %>% semi_join(genes)
-ctcfs <- read_csv("../data/ctcfbs.csv", 
-                  col_names = c("ID", "specie", "chr", "cell", "exp", "source"), skip = 1)
-ctcfs <- ctcfs %>% select("chr")
-ctcfs <- ctcfs %>% separate(col = "chr", into = c("chr", "pos"), sep = ":")
-ctcfs <- ctcfs %>% separate(col = "pos", into = c("start", "end"), sep = "-", convert = T)
-ctcfs$chr <- str_replace(ctcfs$chr, "chr", "")
-
+genes <- read_tsv("data/genes_in_exp_matrix.txt", col_names = "ensembl_id")
 chrs <- as.character(c(seq(1:22), "X"))
 
-nov_ctcfs <- parallel::mclapply(X = chrs, mc.cores = 5, FUN = function(c){
-  gs_in_c <- annot %>% filter(chr == c)
-  cs_in_c <- ctcfs %>% filter(chr == c)
+annot <- annot %>% semi_join(genes)
+ctcfs <- read_tsv("data/GSE85108/GSM2257816_CTCF_E0h_MCF7_peaks.bed", 
+                  col_names = c("chr", "start", "end", "id", "score"))
+ctcfs <- ctcfs %>% mutate(chr = str_replace(chr, "chr", "")) %>% 
+  filter(chr %in% chrs)
+                            
+nov_ctcfs <- parallel::mclapply(X = chrs, mc.cores = 23, FUN = function(ch){
+  gs_in_c <- annot %>% filter(chr == ch)
+  cs_in_c <- ctcfs %>% filter(chr == ch)
   
-  cs <- apply(cs_in_c, MARGIN = 1, function(row){
+  cs <- lapply(1:nrow(cs_in_c), function(row){
+    row <- cs_in_c[row, ]
     ends <- gs_in_c$end >= row["start"] & gs_in_c$end <= row["end"]
     starts <- gs_in_c$start >= row["start"] & gs_in_c$start <= row["end"]
     if(!(any(ends) | any(starts))) {
@@ -39,4 +36,4 @@ nov_ctcfs <- parallel::mclapply(X = chrs, mc.cores = 5, FUN = function(c){
 
 nov_ctcfs <- ldply(nov_ctcfs)
                      
-write_tsv(nov_ctcfs, path = "../data/non_gene_overlaping_ctcfbs.tsv")
+write_tsv(nov_ctcfs, path = "data/non_gene_overlaping_ctcfbs.tsv")
