ba39f118f426aa1740cd7d4004a715b5d96dee96
Diana Elisa García Cortés dgarcia@inmegen.edu.mx <dgarcia@inmegen.edu.mx>
2020-07-21 19:45:01 -0500

    changing figure size

--- 4c_getCommunitiesSizePlot.R
+++ 4c_getCommunitiesSizePlot.R
@@ -5,13 +5,13 @@ library(ggplot2)
 library(ggthemes)
 
 conds <- c("healthy", "luma")
-label_conds <- c("Healthy", "Luma")
+label_conds <- c("Healthy", "LumA")
 names(label_conds) <- conds
 colors <- c("#e3a098", "#a32e27")
-colors <- c("#C7C7C7", "#FF9E4A")
+#colors <- c("#C7C7C7", "#FF9E4A")
 
 all_comm_info <- lapply(conds, function(cond){
-  comm_info <- read_tsv(paste0("data/", cond, "-communities-info.tsv"))
+  comm_info <- read_tsv(paste0("data/communities/", cond, "-communities-info.tsv"))
   comm_info$cond <- label_conds[cond]
   return(comm_info)
 })
@@ -34,6 +34,6 @@ p <- all_comm_info %>%
   xlab("Number of nodes in community") +
   ylab("Frequency")
 
-png(paste0("figures/communities/community-order-by-cond.png"), width = 800, height = 800)
+png(paste0("figures/communities/community-order-by-cond.png"), width = 1200, height = 800)
 p
 dev.off()  
