2b143a6e851f8973f994d6d70d1b00305571a56e
Diana Elisa García Cortés dgarcia@inmegen.edu.mx <dgarcia@inmegen.edu.mx>
2020-07-24 21:40:19 -0500

    Updating data for alluvial

--- 10_compareEnrichments.R
+++ 10_compareEnrichments.R
@@ -53,11 +53,14 @@ inter_enrich <-  luma_enr %>% filter(community_type == "Inter-chromosomal")
 ## 792 términos en 20 comunidades inter
 inter_enrich %>% filter(commun == 230) %>% select(ID) %>% write_tsv("data/enrich-universe/comm230-inter.tsv")
 
+
 luma_comm_info <- read_tsv("data/communities/luma-communities-info.tsv")
 luma_vertices <- read_tsv("data/network-tables/luma-20127-vertices.tsv", 
                           col_types = cols_only(ensemblID = col_character(), symbol = col_character()))
 
 luma_comm_info <- luma_comm_info %>% left_join(luma_vertices, by = c("pg_gene" = "ensemblID"))
+#luma_comm_info[luma_comm_info$com_id == 158, "symbol"] <- "FOXM1"
+#luma_comm_info %>% filter(com_id == 158)
 luma_enr %>% left_join(luma_comm_info %>% select(com_id, symbol), by = c("commun" = "com_id")) %>% 
   select(ID, symbol, community_type)%>% unique() %>%
   write_tsv("data/enrich-universe/id-comm-type-for-alluvial.tsv")
