301dee9939a455ac07f2bfe83be4b96f2f363336
Diana García <dgarcia@inmegen.edu.mx>
2021-02-17 19:20:01 -0600

    Getting communities and plots for leading eigenvector, infomap, louvain and fast greedy algorithms

--- 4_getCommunities.R
+++ 4_getCommunities.R
@@ -5,6 +5,7 @@
 library(igraph)
 library(readr)
 library(dplyr)
+library(ggplot2)
 
 getComInfo <- function(cmembership, network){
   comp.info <- lapply(unique(cmembership), function(idc){
@@ -26,29 +27,57 @@ getComInfo <- function(cmembership, network){
 n <- 20127  
 conds <- c("healthy", "luma")
 
-m <- lapply(conds, function(cond) {
-  interactions <- read_tsv(file = paste0("data/network-tables/", cond, "-", n, "-interactions.tsv"))
+functions <-  c(cluster_leading_eigen, cluster_fast_greedy, cluster_louvain, 
+                cluster_infomap)
 
-  vertices <- read_tsv(file = paste0("data/network-tables/", cond, "-", n, "-vertices.tsv"))
-  
-  colnames(interactions)[1:2] <- c("from", "to")
-  net <- graph_from_data_frame(interactions, 
-  directed=F, vertices = vertices)
-  comm <- cluster_louvain(graph = net)
-  names(comm$membership) <- comm$names
-  
-  df_comm <- data.frame(comm$names, comm$membership)
-  colnames(df_comm) <- c("ensemblID", "community")
-  
-  vertices <- vertices %>% inner_join(df_comm, by = "ensemblID")
-  
-  write.table(vertices, file = paste0("data/network-tables/", cond, "-", n, "-vertices.tsv") , 
-              quote = F, row.names = F, col.names = T, sep = "\t")
-  
-  write.table(df_comm, paste0("data/communities/", cond,  "-communities.tsv"), 
-            quote = F, row.names = F, col.names = T, sep = "\t")
-  
-  comm_info <- getComInfo(comm$membership, net)
-  write.table(comm_info, paste0("data/communities/", cond, "-communities-info.tsv"), 
-              quote = F, row.names = F, col.names = T, sep = "\t")
+m <- lapply(functions, function(f) {
+  comms <- lapply(conds, function(cond) {
+      
+      interactions <- read_tsv(file = paste0("data/network-tables/", cond, "-", n, 
+                                             "-interactions.tsv"))
+    
+      vertices <- read_tsv(file = paste0("data/network-tables/", cond, "-", n,
+                                         "-vertices.tsv"))
+      
+      colnames(interactions)[1:3] <- c("from", "to", "weight")
+      net <- graph_from_data_frame(interactions, directed=F, vertices = vertices)
+      
+      comm <- f(graph = net)
+      algrthm <- gsub(" ", "_", comm$algorithm)
+      print(paste0("Testing function: ", algrthm))
+      names(comm$membership) <- comm$names
+      
+      df_comm <- data.frame(comm$names, comm$membership)
+      colnames(df_comm) <- c("ensemblID", "community")
+      
+      vertices <- vertices %>% inner_join(df_comm, by = "ensemblID")
+      
+
+      write.table(df_comm, paste0("data/communities/", cond,  "-communities-", 
+                                  algrthm, ".tsv"), quote = F, row.names = F,
+                  col.names = T, sep = "\t")
+
+      comm_info <- getComInfo(comm$membership, net)
+      comm_info$cond <- cond 
+      write.table(comm_info, paste0("data/communities/", cond, "-communities-info-",
+                                    algrthm, ".tsv"), quote = F, row.names = F, 
+                  col.names = T, sep = "\t")
+      
+      return(list(cond = cond, modularity = modularity(comm), algorithm = algrthm))
+  })
+  mod <- bind_rows(comms)
+  return(mod)
 })
+m <- bind_rows(m)
+m
+# # A tibble: 8 x 3
+# cond    modularity algorithm          
+# <chr>        <dbl> <chr>              
+# 1 healthy      0.696 leading_eigenvector
+# 2 luma         0.892 leading_eigenvector
+# 3 healthy      0.703 fast_greedy        
+# 4 luma         0.934 fast_greedy        
+# 5 healthy      0.752 multi_level        
+# 6 luma         0.935 multi_level        
+# 7 healthy      0.674 infomap            
+# 8 luma         0.907 infomap  
--- 4c_getCommunitiesSizePlot.R
+++ 4c_getCommunitiesSizePlot.R
@@ -8,32 +8,64 @@ conds <- c("healthy", "luma")
 label_conds <- c("Healthy", "LumA")
 names(label_conds) <- conds
 colors <- c("#e3a098", "#a32e27")
-#colors <- c("#C7C7C7", "#FF9E4A")
-
-all_comm_info <- lapply(conds, function(cond){
-  comm_info <- read_tsv(paste0("data/communities/", cond, "-communities-info.tsv"))
-  comm_info$cond <- label_conds[cond]
-  return(comm_info)
+names(colors) <- conds
+algorithms <- c("Fast Greedy", "Infomap", "Leading Eigenvector", 
+                "Louvain")
+names(algorithms) <- c( "fast_greedy", "infomap", "leading_eigenvector", "multi_level")
+m <- lapply(conds, function(cond){
+  print(cond)
+  
+  all_comm_info <- lapply(names(algorithms), function(algrthm){
+    comm_info <- read_tsv(paste0("data/communities/", cond, "-communities-info-",
+                                 algrthm, ".tsv"), col_types = cols(chr = col_character()))
+    comm_info$algrthm <- algorithms[algrthm]
+    comm_info$cond <- cond
+    return(comm_info)
+  })
+  
+  all_comm_info <- bind_rows(all_comm_info)
+  label_size <- c("1-2","3-4", "5-9", "10-19", "20-49", "50-99", "100-199",
+                  "200-500", ">500")
+    
+  p <- all_comm_info %>% 
+    group_by(algrthm, gr = cut(order, 
+                            breaks = c(0, 3, 5, 10, 20, 50, 100, 200, 500, 
+                                       max(all_comm_info$order)+1),
+                            labels = label_size,
+                            right = FALSE)) %>% 
+    summarise(nnodes = n()) %>%
+    pivot_wider(names_from = "gr", values_from = "nnodes", values_fill = NA) %>%
+    pivot_longer(cols = !algrthm, names_to = "gr", values_to = "nnodes") %>%
+    mutate(gr = factor(gr, levels = label_size)) %>%
+    arrange(gr) %>% 
+    ggplot( aes(y = nnodes, x = gr, fill=cond, color = cond)) +
+    geom_bar(position=position_dodge(width=1), stat="identity") +
+    scale_fill_manual(values = colors[cond]) +
+    scale_color_manual(values = colors[cond]) +
+    theme_base()  +
+    theme(legend.position = "none") +
+    xlab("Number of nodes in community") +
+    ylab("Frequency") +
+    facet_wrap(~algrthm, ncol = 1, labeller = label_value)
+  
+  png(paste0("figures/communities/community-order-by-algorithm-",cond,"-bars.png"), 
+      width = 600, height = 800)
+  print(p)
+  dev.off()  
+  
+  p <- all_comm_info %>% 
+    filter(order >= 3) %>%
+    ggplot( aes(y = order, x = algrthm, fill=cond, color = cond)) +
+    geom_violin() +
+    scale_fill_manual(values = colors[cond]) +
+    scale_color_manual(values = colors[cond]) +
+    theme_base()  +
+    theme(legend.position = "none") +
+    ylab("Number of nodes in community") 
+  
+  png(paste0("figures/communities/community-order-by-algorithm-", cond, "-violin.png"), 
+      width = 600, height = 400)
+  print(p)
+  dev.off()  
+  
 })
-all_comm_info <- bind_rows(all_comm_info)
-all_comm_info %>% group_by(cond) %>% filter( order > 200) 
-
-p <- all_comm_info %>% 
-  group_by(cond, gr = cut(order, 
-                          breaks = c(0, 5, 50, 100, 200, 400, 800),
-                          labels = c("1-5", "6-49", "50-99", "100-199", "200-399", "400-799"))) %>% 
-  summarise(nnodes = n()) %>%
-  arrange(as.numeric(gr)) %>% 
-  ggplot( aes(y = nnodes, x = gr, fill=cond, color = cond)) +
-  geom_bar(position=position_dodge(width=1), stat="identity") +
-  scale_fill_manual(values = colors) +
-  scale_color_manual(values = colors) +
-  theme_base()  +
-  theme(legend.title = element_blank()) +
-  #facet_wrap(~cond, nrow = 2) +
-  xlab("Number of nodes in community") +
-  ylab("Frequency")
-
-png(paste0("figures/communities/community-order-by-cond.png"), width = 1200, height = 800)
-p
-dev.off()  
